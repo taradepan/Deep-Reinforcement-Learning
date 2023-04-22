a86f692ac9522ccde2d522d1defb1eecf1d195f3
taradepan <taradepan1313@gmail.com>
2023-04-22 23:09:22 +0530

    Create DRL.py

---
+++ DRL.py
@@ -0,0 +1,50 @@
+import random
+import numpy as np
+import gym
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers.legacy import Adam
+from rl.agents import DQNAgent
+from rl.policy import BoltzmannQPolicy
+from rl.memory import SequentialMemory
+
+env = gym.make('CartPole-v0')
+
+states = env.observation_space.shape[0]
+actions = env.action_space.n
+
+model = Sequential()
+model.add(Flatten(input_shape=(1, states)))
+model.add(Dense(24, activation='relu'))
+model.add(Dense(24, activation='relu'))
+model.add(Dense(actions, activation='linear'))
+
+agent = DQNAgent(model=model,
+                memory=SequentialMemory(limit=50000, window_length=1), 
+                policy=BoltzmannQPolicy(), 
+                nb_actions=actions, 
+                nb_steps_warmup=10, 
+                target_model_update=0.01
+                )
+agent.compile(Adam(lr=1e-3), metrics=['mae'])
+agent.fit(env, nb_steps=50000, visualize=True, verbose=1)
+results = agent.test(env, nb_episodes=10, visualize=True)
+
+print(np.mean(results.history['episode_reward']))
+
+env.close()
+
+# episodes = 10
+# for episode in range(1, episodes+1):
+#     state = env.reset()
+#     done = False
+#     score = 0
+
+#     while not done:
+#         action = random.choice([0, 1])
+#         _, reward, done, _ = env.step(action)
+#         score += reward
+#         env.render()
+
+#     print('Episode:{} Score:{}'.format(episode, score))
+# env.close()
